5913f09b0e7013476cce7126ebed92ecba7043f3
sshb_online <sunshebin@gmail.com>
2026-05-29 09:36:32 +0800

    update build scripts and add se single app

--- build.rs
+++ build.rs
@@ -11,5 +11,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             &["proto"],
         )?;
 
+    let se_app_proto = manifest_dir.join("apps/software_engineering_single/proto/se_app.proto");
+    let se_app_proto_dir = manifest_dir.join("apps/software_engineering_single/proto");
+
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(false)
+        .compile(
+            &[se_app_proto.to_str().unwrap()],
+            &[se_app_proto_dir.to_str().unwrap()],
+        )?;
+
+    println!("cargo:rerun-if-changed=proto/pdca_core.proto");
+    println!("cargo:rerun-if-changed=apps/software_engineering_single/proto/se_app.proto");
+
     Ok(())
 }
